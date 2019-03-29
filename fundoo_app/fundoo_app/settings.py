cda1c0c54e91f4ea0ef4b8b98995d1e6773613a3
kirankumar242018 <46438444+kirankumar242018@users.noreply.github.com>
2019-03-29 14:59:20 +0530

    Update settings.py

--- fundoo_app/fundoo_app/settings.py
+++ fundoo_app/fundoo_app/settings.py
@@ -223,7 +223,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kiranchallengingstar@gmail.com'
-EMAIL_HOST_PASSWORD = '8123996968'
+EMAIL_HOST_PASSWORD = '**********'
 EMAIL_PORT = 587
 
 
